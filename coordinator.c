7295ef578895bb333e6eb16d1c32e4dbe6960613
zheng <zhengmatteo2005@gmail.com>
2026-08-09 20:31:31 +0200

    aggiunto gestione SIGINT, chiusura thread tramite pthread_join()

--- coordinator.c
+++ coordinator.c
@@ -16,14 +16,45 @@ direttore di orchestra.
 #include <stdio.h>  //util
 #include <stdlib.h> //util
 #include <unistd.h> //util
-
+#include <string.h>
 
 #include <sys/socket.h> //creazione socket, bind, listen ... 
 #include <netinet/in.h> //preparazione indirizzo
 
 #include <pthread.h>
 
+#include <signal.h>
+#include <errno.h>
+
+#define MAX_THREADS 1024
+
+//flag settato da handler di SIGINT
+volatile sig_atomic_t shutdown_requested = 0;
+
+//array che tiene traccia di tutti i thread attivi
+pthread_t thread_ids[MAX_THREADS];
+int thread_count = 0;
+pthread_mutex_t thread_list_mutex = PTHREAD_MUTEX_INITIALIZER; //lock per accesso all'array dei thread
+
+int listen_fd_global = -1;
+
+void sigint_handler(int signo){
+    (void) signo;
+    shutdown_requested = 1;
+    if (listen_fd_global != -1){ //(listen fd era aperto)
+        close(listen_fd_global);
+    }
+}
+
 int main(void){
+    //registrazione handler SIGINT
+    struct sigaction sa;
+    memset(&sa, 0, sizeof(sa)); //pulisci memoria
+    sa.sa_handler = sigint_handler; //collega handler
+    sigemptyset(&sa.sa_mask); //imposta sa_mask come vuoto, quindi accetta tutti i segnali durante eseuzione di sigint_handler
+    sa.sa_flags = 0; //nessun flag speciale
+    sigaction(SIGINT, &sa, NULL);   //usa sa per la gestione di SIGINT
+
     //creazione socket lato server, sempre in ascolto
     int listen_fd = socket(AF_INET, SOCK_STREAM, 0); //AF_INET: usa ipv4, SOCK_STREAM: usa tcp, 0: protocollo scelto automaticamente
     if (listen_fd < 0) {
@@ -31,6 +62,7 @@ int main(void){
         exit(1);
     }
     printf("server socket creato correttamente. fd = %d\n", listen_fd);
+    listen_fd_global = listen_fd;
 
     //preparazione indirizzo socket
     struct sockaddr_in addr = {0};
@@ -38,7 +70,7 @@ int main(void){
     addr.sin_addr.s_addr = INADDR_ANY;  // indirizzo ip su cui il server ascolterà. IDADDR_ANY = 0.0.0.0
     addr.sin_port = htons(PORT); //porta su cui server sta in ascolto. htons() = host to network short, converte la porta al formato della rete.
 
-    // //SO_REUSEADDR, riutilizzo porta e indirizzo
+    //SO_REUSEADDR, riuso rapido porta
     int opt = 1; //abilita SO_REUSEADDR
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -60,11 +92,14 @@ int main(void){
     printf("coordinator in ascolto sulla porta %d... \n", PORT);
 
 
-
-    while (1){ //loop per continuare ad accettare connessioni
+    //loop principale, continua ad accettare connessioni affinché non si ha SIGINT
+    while (!shutdown_requested){ 
         //accept(), accetta connessioni arrivate al socket.
         int client_fd = accept(listen_fd, NULL, NULL);
         if (client_fd < 0){
+            if (shutdown_requested){
+                break;  //se accept interrotto da SIGINT esci direttamente
+            }
             perror("server accept() fallito");
             continue;   //se accept fallisce prova con la prossima connessione
         }
@@ -88,12 +123,28 @@ int main(void){
             continue;
         }
         printf("creato thread %p per il client fd %d\n", (void *)tid, client_fd);
+
+        //per il thread creato aggiungi in lista
+        pthread_mutex_lock(&thread_list_mutex);
+        if (thread_count < MAX_THREADS){
+            thread_ids[thread_count++] = tid;
+        }
+        pthread_mutex_unlock(&thread_list_mutex);
+
+
         
         //libera risorse usate dal thread dopo aver terminato
         pthread_detach(tid); 
     }
 
+    printf("ricevuto SIGINT, chiusra in corso: %d thread attivi...\n", thread_count);
 
-    close(listen_fd);
+    //attendi join di tutti i thread registrati
+    for (int i = 0; i < thread_count; i++){
+        pthread_join(thread_ids[i], NULL);
+    }
+    printf("terminati tutti i thread, coordinator chiuso pulitamente. \n");
+
+    // close(listen_fd);
     return 0;
 }
